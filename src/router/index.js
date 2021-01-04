177474452296e581fdcbd8860c9729ed6fd70ce9
haoruii <1692970302@qq.com>
2021-01-04 22:24:12 +0800

    finish

--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,59 @@ import VueRouter from 'vue-router'
 
 
 Vue.use(VueRouter)
+// 导入需要的组件
+// import Films from '@/views/Films/Index'
+// import NowPlaying from '@/views/Films/NowPlaying'
+// import ComingSoon from '@/views/Films/ComingSoon'
+// import Detail from '@/views/Films/Detail'
+// import Cinemas from '@/views/Cinemas/Index'
+// import Center from '@/views/Center/Index'
+
+// 导入经过模块化的路由
+import filmRouter from './routes/film';
+import cinemaRouter from './routes/cinema';
+import centerRouter from './routes/center';
 
 const routes = [
   {
     path: '/',
-    // name: 'Home',
-    // component: Home
+    redirect:'/films/nowPlaying'
+    
   },
-  {
+  ...filmRouter,
+  cinemaRouter,
+  centerRouter,
+
+  // {
+  //   path:'/films',
+  //   component:Films,
+  //   children:[
+  //     {path:'nowPlaying', component:NowPlaying},
+  //     {path:'comingSoon', component:ComingSoon}
+  //   ],
+  // },
+  // {
+  //   path:'/film/:filmId',
+  //   component:Detail,
+  // },
+  // 影院模块
+  // {
+  //   path:'/cinemas',
+  //   component:Cinemas,
+  // },
+  // 我的模块
+  // {
+  //   path:'/center',
+  //   component:Center
+  // },
+  
     // path: '/about',
     // name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  
 ]
 
 const router = new VueRouter({
